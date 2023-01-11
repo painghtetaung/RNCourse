ba2160c3d9003806641120b8964aa6e65579cd15
painghtetaung <painghtetaung1999@gmail.com>
2023-01-11 17:39:45 +0630

    style testing

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={custumStyles.bb}>Hello Dingers</Text>
+			<Text style={{ backgroundColor: "red", margin: 21 }}>
+				Hello Dingers hello
+			</Text>
+			<Button color="red" title="Tap Me !" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
+
+const custumStyles = StyleSheet.create({
+	bb: {
+		backgroundColor: "blue",
+		margin: 21,
+		borderWidth: 2,
+		borderColor: "blue",
+		padding: 10,
+		color: "#fff",
+	},
 });
