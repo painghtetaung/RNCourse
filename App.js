47e5be94c77f5fd59f3785346faed60a8f56c4d1
painghtetaung <painghtetaung1999@gmail.com>
2023-01-15 19:36:41 +0630

    started to use onChangeTest

--- App.js
+++ App.js
@@ -1,34 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+const goalInputHandler = (enteredText) => {
+	console.log(enteredText);
+};
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={custumStyles.bb}>Hello Dingers</Text>
-			<Text style={{ backgroundColor: "red", margin: 21 }}>
-				Hello Dingers hello
-			</Text>
-			<Button color="red" title="Tap Me !" />
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal!"
+					onChangeText={goalInputHandler}
+				/>
+				<Button title="Add Goal" />
+			</View>
+			<View style={styles.goalsContainer}>
+				<Text>List of goals...</Text>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
+	appContainer: {
 		flex: 1,
-		backgroundColor: "#fff",
+		paddingTop: 50,
+		paddingHorizontal: 16,
+	},
+	inputContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
 		alignItems: "center",
-		justifyContent: "center",
+		paddingBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: "#cccccc",
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: "#cccccc",
+		width: "70%",
+		marginRight: 10,
+		padding: 8,
+	},
+	goalsContainer: {
+		flex: 4,
 	},
 });
 
-const custumStyles = StyleSheet.create({
-	bb: {
-		backgroundColor: "blue",
-		margin: 21,
-		borderWidth: 2,
-		borderColor: "blue",
-		padding: 10,
-		color: "#fff",
-	},
-});
+// const custumStyles = StyleSheet.create({
+// 	bb: {
+// 		backgroundColor: "blue",
+// 		margin: 21,
+// 		borderWidth: 2,
+// 		borderColor: "blue",
+// 		padding: 10,
+// 		color: "#fff",
+// 	},
+// });
